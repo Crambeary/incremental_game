b528e4f04115f8b60e434c8c8edcac1bd7839587
Marc Totaro <marctotaro94@gmail.com>
2025-12-27 16:26:15 -0600

    added harvesting potatoes panel

--- scripts/State.gd
+++ scripts/State.gd
@@ -1,16 +1,13 @@
 extends Node
 
+var TICK_RATE # init value in _ready
+
 var current_fries: float = 0
 var unlock_hire := true
-var TICK_RATE # init value in _ready
 var current_employees := 1
 var hire_cost: int = 100
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var current_potatoes: float = 0
+var unlock_potato_hire := true
+var current_harvesters := 1
+var harvester_cost: int = 100
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -8,23 +8,30 @@ extends Node
 @onready var b_employee: Button = %b_employee
 @onready var employee_container: VBoxContainer = %employee_container
 
+@onready var l_potatoes: Label = %l_potatoes
+@onready var potatos_per_second: Label = %potatos_per_second
+@onready var b_harvest: Button = %b_harvest
+@onready var l_harvest_employees: Label = %l_harvest_employees
+@onready var b_harvesters: Button = %b_harvesters
+
 @onready var tick_timer: Timer = $Tick
 
 # TODO: Move data into a State autoload script, much like the 
 # godot-reactive-ui example
 # TODO: Add a new state of items, maybe harvesting into a basket, 
 # or shipping via trucks
-#var current_fries: float = 0
-#var unlock_hire := true
-#var TICK_RATE # init value in _ready
-#var current_employees := 3
-#var hire_cost: int = 100
+
 var frys_shown: float
+var potatoes_shown: float
 
 func _ready() -> void:
 	b_fry.connect("button_down", fry_time)
 	b_employee.connect("button_down", hire_employee)
 	employee_container.visible = false
+	
+	b_harvest.connect("button_down", harvest_time)
+	b_harvesters.connect("button_down", hire_harvester)
+	
 	State.TICK_RATE = tick_timer.wait_time
 	tick_timer.start(State.TICK_RATE)
 	tick_timer.connect("timeout", tick)
@@ -38,10 +45,20 @@ func _process(delta: float) -> void:
 		employee_container.visible = true
 	per_second.text = str(roundf(State.current_employees * 2), "/s")
 	b_employee.text = str("Hire(", State.hire_cost, ")")
-	l_employees.text = str("Employees: ", State.current_employees)
+	l_employees.text = str("Friers: ", State.current_employees)
+
+	potatoes_shown = lerp(potatoes_shown, State.current_potatoes, 0.06)
+	l_potatoes.text = str(int(roundf(potatoes_shown)), " Potatoes")
+	potatos_per_second.text = str(roundf(State.current_harvesters * 2), "/s")
+	b_harvesters.text = str("Hire(", State.harvester_cost, ")")
+	l_harvest_employees.text = str("Harvesters: ", State.current_harvesters)
+
+
+
 
 func fry_time() -> void:
 	State.current_fries += 10
+
 
 func hire_employee() -> void:
 	# auto-click
@@ -53,6 +70,18 @@ func hire_employee() -> void:
 func employee_tick() -> void:
 	if State.current_employees > 0:
 		State.current_fries += (State.current_employees * 2)
+		
+	if State.current_harvesters > 0:
+		State.current_potatoes += (State.current_harvesters * 2)
 
 func tick() -> void:
 	employee_tick()
+
+func harvest_time() -> void:
+	State.current_potatoes += 10
+	
+func hire_harvester() -> void:
+	if State.current_potatoes >= State.harvester_cost:
+		State.current_potatoes -= State.harvester_cost
+		State.current_harvesters += 1
+		State.harvester_cost = roundi(State.harvester_cost * 1.1)
