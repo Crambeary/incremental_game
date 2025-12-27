02927d4a40150f3acb441350b6f66ae4db3a01b1
Marc <marctotaro94@gmail.com>
2025-12-27 15:47:46 -0600

    global state, smooth increment, harvest panel wip

---
+++ scripts/State.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var current_fries: float = 0
+var unlock_hire := true
+var TICK_RATE # init value in _ready
+var current_employees := 1
+var hire_cost: int = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -14,43 +14,45 @@ extends Node
 # godot-reactive-ui example
 # TODO: Add a new state of items, maybe harvesting into a basket, 
 # or shipping via trucks
-var current_fries: float = 0
-var unlock_hire := true
-var TICK_RATE # init value in _ready
-var current_employees := 3
-var hire_cost: int = 100
+#var current_fries: float = 0
+#var unlock_hire := true
+#var TICK_RATE # init value in _ready
+#var current_employees := 3
+#var hire_cost: int = 100
+var frys_shown: float
 
 func _ready() -> void:
 	b_fry.connect("button_down", fry_time)
 	b_employee.connect("button_down", hire_employee)
 	employee_container.visible = false
-	TICK_RATE = tick_timer.wait_time
-	tick_timer.start(TICK_RATE)
+	State.TICK_RATE = tick_timer.wait_time
+	tick_timer.start(State.TICK_RATE)
 	tick_timer.connect("timeout", tick)
 
 func _process(delta: float) -> void:
-	l_fries.text = str(int(roundf(current_fries)), " Fries")
-	if current_fries >= hire_cost:
-		unlock_hire = true
-	if unlock_hire:
+	frys_shown = lerp(frys_shown, State.current_fries, 0.06)
+	l_fries.text = str(int(roundf(frys_shown)), " Fries")
+	if State.current_fries >= State.hire_cost:
+		State.unlock_hire = true
+	if State.unlock_hire:
 		employee_container.visible = true
-	per_second.text = str(roundf(current_employees * 2), "/s")
-	b_employee.text = str("Hire(", hire_cost, ")")
-	l_employees.text = str("Employees: ", current_employees)
+	per_second.text = str(roundf(State.current_employees * 2), "/s")
+	b_employee.text = str("Hire(", State.hire_cost, ")")
+	l_employees.text = str("Employees: ", State.current_employees)
 
 func fry_time() -> void:
-	current_fries += 1
+	State.current_fries += 10
 
 func hire_employee() -> void:
 	# auto-click
-	if current_fries >= hire_cost:
-		current_fries -= hire_cost
-		current_employees += 1
-		hire_cost = roundi(hire_cost * 1.1)
+	if State.current_fries >= State.hire_cost:
+		State.current_fries -= State.hire_cost
+		State.current_employees += 1
+		State.hire_cost = roundi(State.hire_cost * 1.1)
 
 func employee_tick() -> void:
-	if current_employees > 0:
-		current_fries += (current_employees * 2)
+	if State.current_employees > 0:
+		State.current_fries += (State.current_employees * 2)
 
 func tick() -> void:
 	employee_tick()
